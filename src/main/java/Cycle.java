d3c19c7dcee083df0e41148da57f5d33300d8065
A5Kc2 <kasprzakjak@poczta.onet.pl>
2020-03-02 11:06:25 +0100

    WIP

---
+++ src/main/java/Cycle.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cycle {
+
+    List<Integer> elephants;
+
+    public Cycle() {
+        this.elephants = new ArrayList<>();
+    }
+
+    public List<Integer> getElephants() {
+        return elephants;
+    }
+
+    public void setElephants(List<Integer> elephants) {
+        this.elephants = elephants;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,44 +9,42 @@ public class Main {
 
         int n = 6;
         int[] m = {2400, 2000, 1200, 2400, 1600, 4000};
-        int[] a = {5, 3, 6, 2, 1, 4};
-        int[] b = {2, 4, 6, 1, 5, 3};
+        int[] a = {6, 2, 1, 4, 5, 3};
+        int[] b = {6, 1, 5, 3, 2, 4};
 
         int[] copy = Arrays.copyOf(a, a.length);
-        setElephants(n, copy, b);
+        getCycles(n, copy, b);
         if (Arrays.equals(copy, b)) {
             System.out.println(" SORTED!");
         }
     }
 
-    private static void setElephants(int n, int[] a, int[] b) {
+    private static void getCycles(int n, int[] a, int[] b) {
         boolean[] isProperPosition = new boolean[n];
+        List<Cycle> cycles = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             isProperPosition[i] = a[i] == b[i];
         }
         int c = 0;
         for (int i = 0; i < n; i++) {
             if (!isProperPosition[i]) {
-                c++;
                 int x = i;
                 List<Integer> cycle = new ArrayList<>();
-                int s = 0;
+                c++;
+                cycles.add(new Cycle());
                 while (!isProperPosition[x]) {
                     isProperPosition[x] = true;
-                    if (a[x] != b[x]) {
-                        s++;
-                        cycle.add(b[x]);
-                        cycle.add(a[x]);
-                        x = changePlaces(a, x, b[x]);
-                        System.out.println("Cycle " + c + " ");
-                        System.out.println("Switch number " + s + " ");
-                        System.out.println("Switched numbers so far: " + cycle.toString());
-                        System.out.println("Result so far: " + Arrays.toString(a));
-                    }
+                    cycle.add(b[x]);
+                    cycles.get(c-1).elephants.add(b[x]);
+                    x = changePlaces(a, x, b[x]);
+                    System.out.println("Cycle " + c + " ");
+                    System.out.println("Switched numbers so far: " + cycle.toString());
+                    System.out.println("Result so far: " + Arrays.toString(a));
                     System.out.println(Arrays.toString(isProperPosition));
                 }
             }
         }
+        cycles.forEach(cyc -> System.out.println(cyc.elephants));
     }
 
     private static int changePlaces(int[] array, int i, int y) {
